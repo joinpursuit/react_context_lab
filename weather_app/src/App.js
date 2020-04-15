70d1c9b7fe1ff5f391d71854080278aaa6c72380
dannyl <danilovlaurent@pursuit.org>
2020-04-15 04:08:11 -0400

    style navbar

--- weather_app/src/App.js
+++ weather_app/src/App.js
@@ -1,8 +1,28 @@
 import React from "react";
 import "./Css/reset.css";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
+import NavBar from "./Components/NavBar/NavBar";
+import Home from "./Components/HomePage/Home";
+import Weather from "./Components/Weather/Weather";
+import Clock from "./Components/Clock/Clock";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <NavBar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/weather">
+          <Weather />
+        </Route>
+        <Route exact path="/clock">
+          <Clock />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
