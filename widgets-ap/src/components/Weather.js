c73a17a32c9c9de308bfa0bbe2fd0e0e1f958990
Jovanni <jovanniluna@pursuit>
2020-04-14 21:07:08 -0400

    adds weather forecast to app

--- widgets-ap/src/components/Weather.js
+++ widgets-ap/src/components/Weather.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, {useContext} from "react";
+import { WeatherContext } from "../providers/WeatherProvider";
 
 const Weather = () => {
+    const {forecast} = useContext(WeatherContext);
+
     return(
         <div className="Weather">
-            Weather
+            Weather {forecast}
         </div>
     )
 } 
--- widgets-ap/src/providers/WeatherProvider.js
+++ widgets-ap/src/providers/WeatherProvider.js
@@ -1,17 +1,26 @@
 import React, { createContext, useState } from "react";
+import axios from "axios";
 export const WeatherContext = createContext();
 
 const WeatherProvider = (props) => {
-    const [count, setCount] = useState(0);
+    const [forecast, setForecast] = useState([]);
 
+    const getForecast = async ()=> {
+        debugger
+        try {
+            let res = await axios("http://api.openweathermap.org/data/2.5/forecast?id=5128581&appid=cac8bebe1d460614795313edbd88cb49");
+            
+        } catch (error) {
+            console.log(error)
+        }
+    }
     
 
-    const increment = () => setCount(count + 1);
-    const decrement = () => setCount (count - 1);
+    
     return (
-        <CounterContext.Provider value ={{count, increment, decrement}}>
+        <WeatherContext.Provider value ={{forecast}}>
             {props.children}
-        </CounterContext.Provider>
+        </WeatherContext.Provider>
     )
 }
 
