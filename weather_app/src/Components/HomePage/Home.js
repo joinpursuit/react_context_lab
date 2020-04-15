ccb43c646de855fc56fc565e3dbf0d03f9953723
dannyl <danilovlaurent@pursuit.org>
2020-04-15 13:22:19 -0400

    correct css files

--- weather_app/src/Components/HomePage/Home.js
+++ weather_app/src/Components/HomePage/Home.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="homeDiv">
+      <h1>Welcome!</h1>
+      <p>
+        Widget App is an aplication that allows users to view the current time,
+        and weather in there current location. The user can navigate between the
+        weather and the time via the navigation bar on the left. Also don't
+        forget to try the Dark Mode feature!
+      </p>
+    </div>
+  );
 };
 
 export default Home;
--- weather_app/src/Components/Weather/Weather.js
+++ weather_app/src/Components/Weather/Weather.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Weather = () => {
-  return <div>Weather</div>;
+  return <div></div>;
 };
 
 export default Weather;
