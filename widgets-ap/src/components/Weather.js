a6359d8c637fb9c81e660c528277c5a50487a7c1
Jovanni <jovanniluna@pursuit>
2020-04-15 13:17:15 -0400

    cannot move from provider to page

--- widgets-ap/src/components/Weather.js
+++ widgets-ap/src/components/Weather.js
@@ -2,10 +2,13 @@ import React, {useContext} from "react";
 import { WeatherContext } from "../providers/WeatherProvider";
 
 const Weather = () => {
+    debugger
     const {forecast} = useContext(WeatherContext);
+    const {city} = useContext(WeatherContext);
 
     return(
         <div className="Weather">
+            city{city}
             Weather {forecast}
         </div>
     )
--- widgets-ap/src/providers/WeatherProvider.js
+++ widgets-ap/src/providers/WeatherProvider.js
@@ -1,24 +1,31 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
 import axios from "axios";
 export const WeatherContext = createContext();
 
 const WeatherProvider = (props) => {
     const [forecast, setForecast] = useState([]);
+    const [city, setCity] = useState([])
 
     const getForecast = async ()=> {
         debugger
         try {
             let res = await axios("http://api.openweathermap.org/data/2.5/forecast?id=5128581&appid=cac8bebe1d460614795313edbd88cb49");
-            
+            setForecast(res.data.list);
+            setCity(res.data.city)
         } catch (error) {
             console.log(error)
         }
     }
+
+    useEffect(() => {
+        getForecast()
+    }, [])
+
     
 
     
     return (
-        <WeatherContext.Provider value ={{forecast}}>
+        <WeatherContext.Provider value ={{forecast, city}}>
             {props.children}
         </WeatherContext.Provider>
     )
