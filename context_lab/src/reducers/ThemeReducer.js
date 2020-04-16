cffee5ff8ecf95ca333f650f20214a63e0edcaf9
Marvin Thompson <marvinthompson@pursuit.org>
2020-04-15 22:11:48 -0400

    dispatching the action

--- context_lab/src/reducers/ThemeReducer.js
+++ context_lab/src/reducers/ThemeReducer.js
@@ -3,7 +3,7 @@ import { THEME } from '../actions/actionTypes'
 const ThemeReducer = (state, action) => {
     switch (action.type) {
         case THEME:
-
+            return action.payload
         default:
             return state;
     }
