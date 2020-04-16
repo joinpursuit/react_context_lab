bfc175671807596ae98059c96ab93093f8bfbfe8
maria liza <mariamartinez@pursuit.org>
2020-04-15 20:35:39 -0400

    remove extra node modules

--- context_lab/src/components/Weather.js
+++ context_lab/src/components/Weather.js
@@ -2,7 +2,7 @@ import React from "react";
 import "../css/Weather.css";
 
 const Weather = () => {
-  return <div><h1>Weather</h1></div>;
+  return <div className="weatherDiv"><h1>Weather</h1></div>;
 };
 
 export default Weather;
