2842ae0a917ed3eef102a0993a2f5bf749e7713f
darsuabasi <uduakabasiabasiurua@pursuit.org>
2020-04-16 18:01:56 -0400

    import store

--- weather-app/src/index.js
+++ weather-app/src/index.js
@@ -1,40 +1,23 @@
-// import React from "react";
-// import ReactDOM from "react-dom";
-// import { createStore } from "redux";
-// import { Provider } from "react-redux";
-
-// import rootReducer from './reducers/';
-// import WeatherContainer from '../src/containers/WeatherContainer';
-
-// const store = createStore(rootReducer);
-
-// const App = () => (
-//   <div>
-//     <h2> My Counter App </h2>
-//     <WeatherContainer />
-//   </div>
-// );
-
-// ReactDOM.render(
-//   <Provider store={store}>
-//     <App />
-//   </Provider>,
-//   document.getElementById("root")
-// );
-
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from '../src/App'
 import * as serviceWorker from './serviceWorker';
+
+import { Provider } from 'react-redux'
+import store from '../src/store/store'
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}> 
+    <App/>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
