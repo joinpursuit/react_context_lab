cc835a04a476cdba5fa31b804559a82eba125296
darsuabasi <uduakabasiabasiurua@pursuit.org>
2020-04-17 02:18:51 -0400

    reducers??

--- weather-app/src/reducers/LocationReducer.js
+++ weather-app/src/reducers/LocationReducer.js
@@ -1,3 +1,5 @@
+import { SHOW_LOCATION } from '../actions/actionTypes'
+
 const LocationReducer = (state = [], action) => {
     switch (action.type) {
         case SHOW_LOCATION: 
--- weather-app/src/reducers/WeatherReducer.js
+++ weather-app/src/reducers/WeatherReducer.js
@@ -1,6 +1,8 @@
+import { SHOW_WEATHER } from '../actions/actionTypes'
+
 const WeatherReducer = (state = [], action) => {
     switch (action.type) {
-        case GET_WEATHER: 
+        case SHOW_WEATHER: 
             return [action.payload, ...state]
         default:
             return state;
