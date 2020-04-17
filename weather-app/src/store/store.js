b31fda4514cd736f80fd6131d7d775553d7e4d54
darsuabasi <uduakabasiabasiurua@pursuit.org>
2020-04-17 02:18:08 -0400

    basic css

--- weather-app/src/store/store.js
+++ weather-app/src/store/store.js
@@ -1,10 +1,11 @@
-import thunk from "redux-thunk"; // write action creators -> returns function instead of action
+import thunk from 'redux-thunk'; // write action creators -> returns function instead of action
+import logger from 'redux-logger';
 import { applyMiddleware, createStore } from "redux"; 
 
 //add reducers 
 import reducers from '../reducers/WeatherReducer'
 //middleware
-const middleware = applyMiddleware(thunk)
+const middleware = applyMiddleware(thunk, logger)
 
 //store 
 const store = createStore(reducers, middleware);
