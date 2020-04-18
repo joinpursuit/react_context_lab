d50f5e59e9e0035c26b23a6a70907a1e5fe080e8
darsuabasi <uduakabasiabasiurua@pursuit.org>
2020-04-18 17:40:50 -0400

    weather comp css

--- weather-app/src/components/WeatherComponent.js
+++ weather-app/src/components/WeatherComponent.js
@@ -1,32 +1,32 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import '../css/Weather.css'
 import WeatherForm from '../components/WeatherForm'
 import DisplayWeather from '../components/DisplayWeather'
+import { ThemeContext } from '../providers/ThemeProvider' 
 
 const Weather = () => {
+
+    const { themes } = useContext(ThemeContext);
  
 
     return (
+        <div className={ themes }>
+            <div className="mainDivYo"> 
+                <div className="yurrrDiv">   
+                    <h2> Hello, welcome to the waviest weather app! </h2>
+                        <div className="enterDiv"> 
+                            <h2> Enter a city or state below </h2>
+                                <div className="formDiv">      
+                                    {/* <WeatherForm/> */}
+                                </div>
+                        </div> 
 
-        <div className="mainDivYo"> 
-
-            <div className="yurrrDiv">   
-                <h2> Yurr, welcome to the waviest weather app</h2>
-
-                    <div className="enterDiv"> 
-                        <h2> Enter a city or state below </h2>
-                            <div className="formDiv">      
-                                {/* <WeatherForm/> */}
-                            </div>
-                    </div> 
-
-                    <div className="weatherDisplay">
+                        <div className="weatherDisplay">
                         {/* <DisplayWeather/> */}
-                    </div>
-            
+                        </div>
+                </div>
             </div>
-            
-        </div>
+        </div> 
 
     )
 
