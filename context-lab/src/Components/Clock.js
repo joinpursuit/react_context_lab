c9dc9e8aabeb42102b37996b875ffc62fc9cf9ed
DejaLFlynn <dejaflynn@pursuit.org>
2020-04-18 10:19:03 -0400

    clock component complete

--- context-lab/src/Components/Clock.js
+++ context-lab/src/Components/Clock.js
@@ -1,11 +1,26 @@
 import React from 'react'
-const Clock =()=>{
-    return (
-        <div className="ClockComponent">
 
-           It is 12:00  O' Clock
-        </div>
-    )
+    const Clock = props => {
+        const [date, setDate] = React.useState(new Date());
+       React.useEffect(() => {
+        var timerID = setInterval( () => tick(), 1000 );
+        return function cleanup() {
+            clearInterval(timerID);
+          };
+       });
+      
+         function tick() {
+          setDate(new Date());
+         }
+      
+         return (
+            <div>
+              <h1>Hello, world!</h1>
+              <h2>It is {date.toLocaleTimeString()}.</h2>
+            </div>
+          );
+      
+      
 }
 
 export default Clock
