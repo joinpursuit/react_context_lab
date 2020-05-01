1cf32b9984ee28ba53b2811938ece34d71f07f8c
dannyl <danilovlaurent@pursuit.org>
2020-05-01 19:00:11 -0400

    make homepage and weather page responsive

--- weather_app/src/Components/HomePage/Home.js
+++ weather_app/src/Components/HomePage/Home.js
@@ -3,14 +3,18 @@ import "./CSS/home.css";
 
 const Home = () => {
   return (
-    <div className="homeDiv">
-      <h1>Welcome!</h1>
-      <p>
-        Widget App is an aplication that allows users to view the current time,
-        and weather in there current location. The user can navigate between the
-        weather and the time via the navigation bar on the left. Also don't
-        forget to try the Dark Mode feature!
-      </p>
+    <div className="homeContainer">
+      <div className="homeDiv">
+        <h1>Welcome!</h1>
+        <div className="pContainer">
+          <p id="p">
+            Widget App is an aplication that allows users to view the current
+            time, and weather in there current location. The user can navigate
+            between the weather and the time via the navigation bar on the left.
+            Also don't forget to try the Dark Mode feature!
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
