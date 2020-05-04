6dbe4a82f0f933b88754af780d15caf5338ac993
dannyl <danilovlaurent@pursuit.org>
2020-05-04 03:58:23 -0400

    make app fully responsive

--- weather_app/src/Components/Clock/Clock.js
+++ weather_app/src/Components/Clock/Clock.js
@@ -1,15 +1,14 @@
 import React from "react";
 import "./Css/clock.css";
 import Clock from "react-live-clock";
-
 import Days from "../Days/Days.js";
 
 const Clockk = () => {
   return (
     <div className="clockApp">
-      <div className="clockWrap">
-        <div className="clockContainer">
-          <div className="clockTime">
+      <div className="clockContainer">
+        <div className="clockTime">
+          <div className="clockWrap">
             <div className="towrap" id="clockDays">
               <Days />
             </div>
