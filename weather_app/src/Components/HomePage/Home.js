fd840393919ea462ef41dd3bb4ecf5a2ae79432d
dannyl <danilovlaurent@pursuit.org>
2020-06-15 20:43:38 -0400

    correct spelling errors in about

--- weather_app/src/Components/HomePage/Home.js
+++ weather_app/src/Components/HomePage/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
         <h1>Welcome!</h1>
         <div className="pContainer">
           <p id="p">
-            Widget App is an aplication that allows users to view the current
+            Widget App is an application that allows users to view the current
             time, and weather in there current location. The user can navigate
             between the weather and the time via the navigation bar on the left.
             Also don't forget to try the Dark Mode feature!
